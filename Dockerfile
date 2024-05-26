d9890e08bb008752ce505ec1eb3eeb37a6b7a7e9
Fauzudheen Abdul Hameed <fauzudheen1@gmail.com>
2024-05-26 17:15:07 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:alpine3.20
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . /app
+
+RUN python manage.py collectstatic --no-input
+
+EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ admin_product_management/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from admin_product_management.models import Category, Brand, Product, ProductVariant, Offer
+from admin_product_management.models import Coupon
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+class BrandForm(forms.ModelForm):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+        exclude = [
+            'is_deleted'
+        ]
+
+class ProductVariantForm(forms.ModelForm):
+    class Meta:
+        model = ProductVariant
+        exclude = ['product']
+       
+
+
+class OfferForm(forms.ModelForm):
+    class Meta:
+        model = Offer
+        fields = '__all__'
+        exclude = [
+            'discounted_price'
+        ]
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CouponForm(forms.ModelForm):
+    class Meta:
+        model = Coupon
+        fields = '__all__'
+        widgets = {
+            'expiry_date': forms.DateInput(attrs={'type': 'date'}),
+        }
